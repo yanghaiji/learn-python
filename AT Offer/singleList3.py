8010adc07896b21ec961a3005d9e75ea6b0f8cfc
Yabea <847511885@qq.com>
2018-03-18 15:42:33 +0800

    非递归合并两个排序链表

--- AT Offer/singleList3.py
+++ AT Offer/singleList3.py
@@ -3,6 +3,11 @@
 # 合并两个排序的链表
 from singleLinkedList import List
 
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 class Solution:
     # 返回合并后列表
     def Merge(self, pHead1, pHead2):
