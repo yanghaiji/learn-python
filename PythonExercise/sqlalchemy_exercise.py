167b8f07d9711bbe599cc67a68d5fa13f9c3d9a0
ben <yangbenxiyou@foxmail.com>
2018-05-24 07:31:07 -0700

    sqlalchemy练习

--- PythonExercise/sqlalchemy_exercise.py
+++ PythonExercise/sqlalchemy_exercise.py
@@ -10,9 +10,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import or_, and_
+from sqlalchemy import ForeignKey
 
 # 创建一个SQLORM基类，之后创建的表必须得继承它
-base = declarative_base()
+Modelbase = declarative_base()
 
 # 连接数据库，使用session用于创建程序和数据库之间的会话
 ENGINE = create_engine("mysql+mysqlconnector://root:272825@localhost:3306/testsqlalchemy")
@@ -23,29 +25,44 @@ Session = sessionmaker(bind=ENGINE)
 session = Session()
 
 # 创建Users表
-class Users(base):
+class Users(Modelbase):
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(30), unique=True)
     age = Column(Integer)
+    city_id = Column(Integer, ForeignKey("city.id"))
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
+#创建city表
+class City(Modelbase):
+    __tablename__ = 'city'
+
+    id = Column(Integer, primary_key=True)
+    city = Column(String(30), unique=True)
+
 #初始化数据库
 def init_db():
-    base.metadata.create_all(ENGINE)
+    Modelbase.metadata.create_all(ENGINE)
 
 def drop_db():
-    base.metadata.drop_all(ENGINE)
-
+    Modelbase.metadata.drop_all(ENGINE)
 
 #创建新的User对象
 def insert_users():
     names = {'Jeny':22, 'Lisa':18, "Mark":19}
     for key, value in names.items():
         new_user = Users(key, value)
+        new_user.city_id = 1
         session.add(new_user)
+    session.commit()
+    session.close()
+
+def insert_city():
+    city = City(city="Xian")
+    session.add(city)
     session.commit()
     session.close()
 
@@ -67,7 +84,6 @@ def use_like_query():
     # users = session.query(Users).filter(Users.name.like('M%')).all()
     # users = session.query(Users).filter(Users.name.like('%M%')).filter(Users.name.like('%K%'))
     # users = session.query(Users).filter((Users.name.in_(['Mary','Jeny'])))
-    from sqlalchemy import or_
     users = session.query(Users).filter(or_(Users.name == 'Mark', Users.age == 19)).all()
     # print(session.query(User).filter(User.username.is_(None)).all())
     # print(session.query(User).filter(User.username.isnot(None)).all())
@@ -75,10 +91,28 @@ def use_like_query():
         print item.name
 
 
+def user_lianhe():
+    from sqlalchemy import func
+    users = session.query(Users.name, func.sum(Users.id)).group_by(Users.city_id).all()
+    # for item in users:
+    #     print item.name
+
+
+#update
+def update():
+    from sqlalchemy import update
+    user = session.query(Users).filter(Users.id == 1).update({Users.name, "ben"})
+    session.add(user)
+    session.commit()
+    session.close()
+
+
 if __name__ == "__main__":
     # drop_db()
     # init_db()
+    # insert_city()
     # insert_users()
+    update()
     #简单查询
     # easy_query()
 
@@ -86,7 +120,7 @@ if __name__ == "__main__":
     # where_query()
 
     #使用like
-    use_like_query()
+    # use_like_query()
 
 
 
