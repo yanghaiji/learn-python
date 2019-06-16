2d5a0176bfc58081b6ccf5ea8e1489c4026c397f
Yabea <847511885@qq.com>
2019-06-16 17:27:10 +0800

    numpy基础知识

--- learnnumpy/1.py
+++ learnnumpy/1.py
@@ -1,44 +1,113 @@
-#coding:utf-8
-
-#ndarray对象的学习
-
+# coding:utf-8
+"""
+desc: 基础学习
+"""
 import numpy as np
 
-#创建数组
-a = np.array([1,2,3,4])
-b = np.array([[1,2,3],[2,3,4]])
-print a
-print b
-print a.shape
-print b.shape
-#改变b数组的shape属性
-b.shape = 3,2
-print b
-b.shape = 1, -1
-print b
+# 数组创建
+# a = np.array([1, 2, 3])
+# print(a)
+# print(a.shape)
+# print(a.dtype)
+# print(a.itemsize)
+# print(a.size)
+# print(a.data)
 
-#reshape方法
-c = b.reshape(3,2)
-print c
-print b
+# b = np.zeros(10)
+# c = np.ones((1, 2))
+# print(b)
+# print(c)
 
-#数组b和c共享数组存储内存区域，修改同步
-b[0][1] = 100
-print b
-print c
+# 数组计算
+# i = np.array([[1, 2], [3, 4]])
+# j = np.array([[5, 6], [7, 8]])
+# print(i + j)
+# print(i - j)
+# print(i - 1)
+# print(i * j)
+# print( i / j)
+# print(j.dot(i))
 
-#创建数组的函数
-#arange函数:通过指定开始值、终值和步长来创建一维数组，数组不包括终值
-e = np.arange(0,1,0.2)
-print e
+# 索引和切片
+# arr = np.arange(10)
+# print(arr)
+# print(arr[0])
+# print(arr[1:6])
+# arr_slice = arr[1:6]
+# arr_slice[1:3] = 5
+# print(arr_slice)
+# print(arr)
+# arr_copy = arr[1:6].copy()
+# arr_copy[1:3] = 6
+# print(arr_copy)
+# print(arr)
 
-#linspace函数通过指定开始值、终值和元素个数来创建一维数组，可以通过endpoint指定是否包括终值，缺省表示包括终值。
-f = np.linspace(0,1,10)
-print f
+# arr_2d = np.array([[1, 2, 3], [2, 3, 4], [3, 4, 5]])
+# print(arr_2d[0])
+# print(arr_2d[0, 1])
+# arr_2d_slice = arr_2d[1]
+# print(arr_2d_slice)
+# arr_2d_slice[0] = 1
+# print(arr_2d_slice)
+# print(arr_2d)
 
-#logspace用来创建等比数列
-g = np.logspace(0, 2, 20)
-print g
+# 切片索引
+# arr_test = np.array([[1, 2, 3], [2, 3, 4], [3, 4, 5]])
+# print(arr_test[:2])
+# print(arr_test[:2, 1:])
+# print(arr_test[1, :1])
+# arr_slice_test = arr_test[:2, 1:]
+# arr_slice_test[0] = 0
+# print( arr_slice_test)
+# print(arr_test)
 
+# 布尔索引
+# x = np.array([[0, 1], [2, 3], [3, 4]])
+# print(x)
+# print(x > 1)
+
+# 花式索引
+# array = np.empty((4, 3))
+# for i in range(4):
+#     array[i] = i
+# print(array)
+# print(array[np.ix_([3, 0],[2, 1])])
+
+# 形状转换
+# arr = np.arange(12)
+# print(arr)
+# arr1 = arr.reshape(3, 4)
+#
+# for item in arr1:
+#     print(item)
+# for item in arr1.flat:
+#     print(item)
+#
+# print(arr1.flatten())
+# print(arr1.flatten(order="K"))
+# arr.flatten()[10] = 0
+# print(arr)
+#
+# print(arr.ravel())
+# arr.ravel()[10] = 0
+# print(arr)
+
+# 转置与轴对换
+# arr = np.arange(12).reshape((2, 2, 3))
+# print(arr)
+# print(arr.T)
+# print(arr.transpose((1, 0, 2)))
+# print(arr.swapaxes(1, 2))
+
+
+# 一元func
+# arr = np.arange(10)
+# print(np.sqrt(arr))
+# print(np.square(arr))
+
+# 二元func
+# arr1 = np.arange(10)
+# arr2 = np.arange(10)
+# print(np.add(arr1, arr2))
 
 
